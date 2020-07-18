b2745ed1927a8dcc403ab12b1cf8a1bb5207cc3d
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2020-07-17 22:12:10 -0700

    Add Apple Store model

---
+++ AppleStores/Model/AppleStore.swift
@@ -0,0 +1,22 @@
+//
+//  AppleStore.swift
+//  AppleStores
+//
+//  Created by Tim Roesner on 7/17/20.
+//
+
+import Foundation
+import CoreLocation
+
+struct AppleStore {
+    let name: String
+    let website: URL?
+    let phone: String
+    let location: CLLocation
+    let hours: [Hour]
+}
+
+struct Hour: Codable {
+    let day: String
+    let time: String
+}
