01a0d2ff96d4fda43cf5ae1ed1c3b403b5b77b02
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2020-07-17 22:12:39 -0700

    Add Detail View

---
+++ AppleStores/Views/DetailView.swift
@@ -0,0 +1,22 @@
+//
+//  DetailView.swift
+//  AppleStores
+//
+//  Created by Tim Roesner on 7/17/20.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+// MARK: - Preview
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
