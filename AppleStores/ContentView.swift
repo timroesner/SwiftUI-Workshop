0ea1d27cf0366df4b63dd608593bde107a0e616f
Tim Roesner <13894518+timroesner@users.noreply.github.com>
2020-07-17 21:30:11 -0700

    Initial Commit

---
+++ AppleStores/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  AppleStores
+//
+//  Created by Tim Roesner on 7/17/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
